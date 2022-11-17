6eadad35cbef856fbb6cd5e37c360f8bd764df92
lazarofraga <48263933+lazarofraga@users.noreply.github.com>
2022-11-17 14:45:45 -0500

    Refactor and add FastAPI

---
+++ api/main.py
@@ -0,0 +1,27 @@
+from modules.crypto_module import encrypt, decrypt
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+def validate(cipher):
+    if not decrypt(cipher):
+        return "PADDING ERROR"
+    else:
+        return "VALID CIPHER"
+
+@app.get("/encrypt")
+async def root(message):
+    cipher = encrypt(message)
+    return {"ciphertext": cipher}
+
+@app.get("/decrypt")
+async def root(key, cipher):
+    message = decrypt(key, cipher)
+    return {"message": message}
+
+@app.get("/validate")
+async def root(cipher):
+    message = validate(cipher)
+    return {"status": message}
---
+++ api/modules/crypto_module.py
@@ -0,0 +1,42 @@
+import hashlib
+import base64
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
+from Crypto import Random
+
+mode = AES.MODE_CBC
+block_size = 16
+password = "password123"
+key = hashlib.sha256(password.encode()).digest()
+iv = b'\x81\x915\x1d\xce\x97<|\xd0\x1c\xe2P"\xe2+"'
+
+
+def generate_key(password):
+    return hashlib.sha256(password.encode()).digest()
+
+
+def encrypt(plaintext):
+    cipher = AES.new(key, mode, iv)
+    encrypted_message = cipher.encrypt(pad(plaintext.encode("ascii"), block_size))
+    encrypted_message = base64.urlsafe_b64encode(encrypted_message)
+    return encrypted_message
+
+
+def decrypt(encrypted_message, password=password):
+    print(iv)
+    key = generate_key(password)
+    cipher = AES.new(key, mode, iv)
+    encrypted_message = base64.urlsafe_b64decode(encrypted_message)
+    try:
+        decrypted_text = unpad(cipher.decrypt(encrypted_message), block_size)
+    except ValueError:
+        return False
+    return decrypted_text
+
+
+if __name__ == "__main__":
+    message = "This is your plaintext"
+    print(message)
+    encrypted_message = base64.b64encode(encrypt(key, mode, message))
+    print(encrypted_message)
+    print(decrypt(key, mode, base64.b64decode(encrypted_message)).decode())
